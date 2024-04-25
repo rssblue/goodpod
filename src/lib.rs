5d37050a8445fa56b97ec3cd6bfe356ba04db8b7
Dovydas <dovydas@yoshke.org>
2024-04-25 20:01:06 +0100

    implement publisher feed MVP

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![doc = include_str!("../README.md")]
 
+use uuid::Uuid;
 use xml::attribute::OwnedAttribute;
 use xml::namespace::Namespace;
-use yaserde_derive::YaSerialize as Serialize;
 
 pub struct Rss {
     pub channel: Channel,
@@ -15,9 +15,22 @@ impl yaserde::YaSerialize for Rss {
     ) -> Result<(), String> {
         let data_event = xml::writer::XmlEvent::characters("\n");
         writer.write(data_event).map_err(|e| e.to_string())?;
-        writer
-            .write(xml::writer::XmlEvent::start_element("rss").attr("version", "2.0"))
-            .map_err(|e| e.to_string())?;
+
+        let mut rss_el = xml::writer::XmlEvent::start_element("rss");
+        rss_el = rss_el.attr("version", "2.0");
+
+        if self.channel.itunes_image.is_some() {
+            rss_el = rss_el.ns("itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd");
+        }
+
+        if self.channel.podcast_guid.is_some()
+            || self.channel.podcast_medium.is_some()
+            || self.channel.podcast_remote_items.len() > 0
+        {
+            rss_el = rss_el.ns("podcast", "https://podcastindex.org/namespace/1.0");
+        }
+
+        writer.write(rss_el).map_err(|e| e.to_string())?;
 
         self.channel.serialize(writer)?;
 
@@ -36,9 +49,205 @@ impl yaserde::YaSerialize for Rss {
     }
 }
 
-#[derive(Serialize)]
-#[yaserde(rename = "channel")]
 pub struct Channel {
-    pub title: String,
-    pub description: String,
+    pub description: Option<String>,
+    pub generator: Option<String>,
+    pub last_build_date: Option<chrono::DateTime<chrono::Utc>>,
+    pub title: Option<String>,
+    pub itunes_image: Option<ItunesImage>,
+    pub podcast_guid: Option<Uuid>,
+    pub podcast_medium: Option<String>,
+    pub podcast_remote_items: Vec<PodcastRemoteItem>,
+}
+
+impl yaserde::YaSerialize for Channel {
+    fn serialize<W: std::io::Write>(
+        &self,
+        writer: &mut yaserde::ser::Serializer<W>,
+    ) -> Result<(), String> {
+        writer
+            .write(xml::writer::XmlEvent::start_element("channel"))
+            .map_err(|e| e.to_string())?;
+
+        if let Some(description) = &self.description {
+            writer
+                .write(xml::writer::XmlEvent::start_element("description"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(description))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        if let Some(generator) = &self.generator {
+            writer
+                .write(xml::writer::XmlEvent::start_element("generator"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(generator))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        if let Some(last_build_date) = &self.last_build_date {
+            writer
+                .write(xml::writer::XmlEvent::start_element("lastBuildDate"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(
+                    &last_build_date
+                        .to_rfc2822()
+                        .to_string()
+                        .replace("+0000", "GMT"),
+                ))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        if let Some(title) = &self.title {
+            writer
+                .write(xml::writer::XmlEvent::start_element("title"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(title))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        if let Some(itunes_image) = &self.itunes_image {
+            itunes_image.serialize(writer)?;
+        }
+
+        if let Some(podcast_guid) = &self.podcast_guid {
+            writer
+                .write(xml::writer::XmlEvent::start_element("podcast:guid"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(&podcast_guid.to_string()))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        if let Some(podcast_medium) = &self.podcast_medium {
+            writer
+                .write(xml::writer::XmlEvent::start_element("podcast:medium"))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::characters(podcast_medium))
+                .map_err(|e| e.to_string())?;
+            writer
+                .write(xml::writer::XmlEvent::end_element())
+                .map_err(|e| e.to_string())?;
+        }
+
+        for remote_item in &self.podcast_remote_items {
+            remote_item.serialize(writer)?;
+        }
+
+        writer
+            .write(xml::writer::XmlEvent::end_element())
+            .map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+
+    fn serialize_attributes(
+        &self,
+        attributes: Vec<OwnedAttribute>,
+        namespace: Namespace,
+    ) -> Result<(Vec<OwnedAttribute>, Namespace), String> {
+        Ok((attributes, namespace))
+    }
+}
+
+impl Default for Channel {
+    fn default() -> Self {
+        Self {
+            description: None,
+            generator: None,
+            last_build_date: None,
+            title: None,
+            itunes_image: None,
+            podcast_guid: None,
+            podcast_medium: None,
+            podcast_remote_items: Vec::new(),
+        }
+    }
+}
+
+pub struct ItunesImage {
+    pub href: String,
+}
+
+impl yaserde::YaSerialize for ItunesImage {
+    fn serialize<W: std::io::Write>(
+        &self,
+        writer: &mut yaserde::ser::Serializer<W>,
+    ) -> Result<(), String> {
+        let mut el = xml::writer::XmlEvent::start_element("itunes:image");
+        el = el.attr("href", &self.href);
+
+        writer.write(el).map_err(|e| e.to_string())?;
+        writer
+            .write(xml::writer::XmlEvent::end_element())
+            .map_err(|e| e.to_string())
+    }
+
+    fn serialize_attributes(
+        &self,
+        attributes: Vec<OwnedAttribute>,
+        namespace: Namespace,
+    ) -> Result<(Vec<OwnedAttribute>, Namespace), String> {
+        Ok((attributes, namespace))
+    }
+}
+
+pub struct PodcastRemoteItem {
+    pub feed_guid: Uuid,
+    pub item_guid: Option<String>,
+    pub medium: Option<String>,
+}
+
+impl yaserde::YaSerialize for PodcastRemoteItem {
+    fn serialize<W: std::io::Write>(
+        &self,
+        writer: &mut yaserde::ser::Serializer<W>,
+    ) -> Result<(), String> {
+        let mut el = xml::writer::XmlEvent::start_element("podcast:remoteItem");
+
+        let binding = self.feed_guid.to_string();
+        el = el.attr("feedGuid", &binding);
+
+        if let Some(item_guid) = &self.item_guid {
+            el = el.attr("itemGuid", item_guid);
+        }
+
+        if let Some(medium) = &self.medium {
+            el = el.attr("medium", medium);
+        }
+
+        writer.write(el).map_err(|e| e.to_string())?;
+
+        writer
+            .write(xml::writer::XmlEvent::end_element())
+            .map_err(|e| e.to_string())
+    }
+
+    fn serialize_attributes(
+        &self,
+        attributes: Vec<OwnedAttribute>,
+        namespace: Namespace,
+    ) -> Result<(Vec<OwnedAttribute>, Namespace), String> {
+        Ok((attributes, namespace))
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use goodpod::*;
 use pretty_assertions::assert_eq;
 
@@ -5,12 +6,66 @@ use pretty_assertions::assert_eq;
 fn serialize_simple_feed() {
     let rss = Rss {
         channel: Channel {
-            title: "My title".to_string(),
-            description: "My description".to_string(),
+            title: Some("My title".to_string()),
+            description: Some("My description".to_string()),
+            generator: Some("RSS Blue v1.0".to_string()),
+            last_build_date: None,
+            ..Default::default()
         },
     };
 
     let expected = include_str!("../tests/data/simple-feed.xml").trim();
+    let actual = yaserde::ser::to_string_with_config(
+        &rss,
+        &yaserde::ser::Config {
+            perform_indent: true,
+            indent_string: Some("  ".to_string()),
+            write_document_declaration: true,
+        },
+    )
+    .unwrap();
+
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn serialize_publisher_feed() {
+    let rss = Rss {
+        channel: Channel {
+            title: Some("John Doe".to_string()),
+            description: Some("John Doe is a fictional artist.".to_string()),
+            last_build_date: Some(
+                // Construct using year, month, day, hour, minute, second
+                chrono::prelude::Utc
+                    .with_ymd_and_hms(2024, 4, 20, 0, 5, 50)
+                    .unwrap(),
+            ),
+            itunes_image: Some(ItunesImage {
+                href: "https://example.com/john-doe.jpg".to_string(),
+            }),
+            podcast_guid: Some(
+                uuid::Uuid::parse_str("4136005d-a5b9-534d-b32a-e88894e1ae4a").unwrap(),
+            ),
+            podcast_medium: Some("publisher".to_string()),
+            podcast_remote_items: vec![
+                PodcastRemoteItem {
+                    feed_guid: uuid::Uuid::parse_str("b9f0b2e5-6a56-5e73-83e4-af1c33769e73")
+                        .unwrap(),
+                    item_guid: None,
+                    medium: Some("music".to_string()),
+                },
+                PodcastRemoteItem {
+                    feed_guid: uuid::Uuid::parse_str("cb6e27c5-6d11-54a1-bf60-40537c0336e3")
+                        .unwrap(),
+                    item_guid: None,
+                    medium: Some("music".to_string()),
+                },
+            ],
+            ..Default::default()
+        },
+    };
+
+    let expected = include_str!("../tests/data/publisher-feed.xml").trim();
     let actual = yaserde::ser::to_string_with_config(
         &rss,
         &yaserde::ser::Config {
